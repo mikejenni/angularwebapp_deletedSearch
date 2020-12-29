0fbba8514c542621daadbc79b8302240c94581e9
Yanik Schuppli <yanik.schuppli@gmail.com>
2020-12-29 02:44:15 +0100

    First

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { FirstComponent } from '../app/first/first.component';
+import { SecondComponent } from '../app/second/second.component';
 
-const routes: Routes = [];
+const routes: Routes = [ // sets up routes constant where you define your routes
+  { path: 'first-component', component: FirstComponent },
+  { path: 'second-component', component: SecondComponent },
+];
 
+// configures NgModule imports and exports
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
